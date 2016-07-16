e62651add014c97dbdb75c4fbea25b4c74670fde
abhishekori <abhikori1994@gmail.com>
2016-07-17 00:36:35 +0530

    got text and set it to a text view then converted into image and stored in external storage

--- java/turing/com/picstatus/MainActivity.java
+++ java/turing/com/picstatus/MainActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.icu.text.SimpleDateFormat;
+import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -16,7 +17,9 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -28,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     EditText tv;
     ImageView img;
     Button btn;
+    TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,47 +40,103 @@ public class MainActivity extends AppCompatActivity {
         tv= (EditText) findViewById(R.id.textm);
         img= (ImageView) findViewById(R.id.imageView);
         btn= (Button) findViewById(R.id.button);
+        tv1= (TextView) findViewById(R.id.textView);
+
+        tv.setDrawingCacheEnabled(true);
+        tv.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
+                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+
+        tv.layout(0, 0, tv.getMeasuredWidth(), tv.getMeasuredHeight());
+        tv.buildDrawingCache(true);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Bitmap res = getBitmapFromView(tv);
-                img.setImageBitmap(res);
-                //storeImage(res);
-                String path = Environment.getExternalStorageDirectory().toString();
+                tv1.setText(tv.getText().toString());
+                Bitmap res = getBitmapFromView(tv1);
+               img.setImageBitmap(res);
+img.buildDrawingCache();
+                Bitmap bm=img.getDrawingCache();
+
                 OutputStream fOut = null;
-                File file = new File(path, "FitnessGir.jpg"); // the File to save to
+                Uri outputFileUri;
                 try {
-                    fOut = new FileOutputStream(file);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
+                    File root = new File(Environment.getExternalStorageDirectory()
+                            + File.separator + "folder_name" + File.separator);
+                    root.mkdirs();
+                    File sdImageMainDirectory = new File(root, "myPicName.jpg");
+                    outputFileUri = Uri.fromFile(sdImageMainDirectory);
+                    fOut = new FileOutputStream(sdImageMainDirectory);
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "Error occured. Please try again later.",
+                            Toast.LENGTH_SHORT).show();
                 }
 
-                Bitmap pictureBitmap = res; // obtaining the Bitmap
-                pictureBitmap.compress(Bitmap.CompressFormat.JPEG, 0, fOut); // saving the Bitmap to a file compressed as a JPEG with 85% compression rate
                 try {
+                    bm.compress(Bitmap.CompressFormat.PNG, 0, fOut);
                     fOut.flush();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    fOut.close(); // do not forget to close the stream
-                } catch (IOException e) {
-                    e.printStackTrace();
+                    fOut.close();
+                } catch (Exception e) {
                 }
 
-                try {
-                    MediaStore.Images.Media.insertImage(getContentResolver(), file.getAbsolutePath(), file.getName(), file.getName());
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
+              // cimage(img);
+
+
+
+
+//                Bitmap res = getBitmapFromView(tv);
+//                img.setImageBitmap(res);
+//                //storeImage(res);
+//                String path = Environment.getExternalStorageDirectory().toString();
+//                OutputStream fOut = null;
+//                File file = new File(path, "FitnessGir.jpg"); // the File to save to
+//                try {
+//                    fOut = new FileOutputStream(file);
+//                } catch (FileNotFoundException e) {
+//                    e.printStackTrace();
+//                }
+//
+//                Bitmap pictureBitmap = res; // obtaining the Bitmap
+//                pictureBitmap.compress(Bitmap.CompressFormat.JPEG, 0, fOut); // saving the Bitmap to a file compressed as a JPEG with 85% compression rate
+//                try {
+//                    fOut.flush();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//                try {
+//                    fOut.close(); // do not forget to close the stream
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//
+//                try {
+//                    MediaStore.Images.Media.insertImage(getContentResolver(), file.getAbsolutePath(), file.getName(), file.getName());
+//                } catch (FileNotFoundException e) {
+//                    e.printStackTrace();
+//                }
             }
         });
 
     }
 
 
+public void cimage(View view){
+    Bitmap b = Bitmap.createBitmap(view.getDrawingCache());
+    view.setDrawingCacheEnabled(false);
+    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+    b.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
 
+    File f = new File(Environment.getExternalStorageDirectory() + File.separator + "v2i.jpg");
+    try {
+        f.createNewFile();
+        FileOutputStream fo = new FileOutputStream(f);
+        fo.write(bytes.toByteArray());
+        fo.close();
+    } catch (Exception e) {
+    }
+    //finish();
+
+}
     public static Bitmap getBitmapFromView(View view) {
         Bitmap returnedBitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(),Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(returnedBitmap);
